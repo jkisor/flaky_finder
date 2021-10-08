58e1b647976509333b38ecafab7443fc3ce48508
John Kisor <jkisor@gmail.com>
2021-10-08 08:13:29 -0400

    auto-commit

--- spec/flakey_finder_spec.rb
+++ spec/flakey_finder_spec.rb
@@ -6,20 +6,20 @@ describe FlakeyFinder do
 
   context "when tests pass constantly" do
     it "finds no flakey tests" do
-      expect(subject.find("./flakey_spec/constant_pass_spec.rb")).to eq([])
+      expect(subject.find("./support_spec/constant_pass_spec.rb")).to eq([])
     end
   end
 
   context "when tests fail constantly" do
 
     it "finds no flakey tests" do
-      expect(subject.find("./flakey_spec/constant_pass_spec.rb")).to eq([])
+      expect(subject.find("./support_spec/constant_pass_spec.rb")).to eq([])
     end
   end
 
   context "when tests fail sometimes" do
     it "finds a flakey test" do
-      expect(subject.find("./flakey_spec/flakey_spec.rb")).to eq([ "./flakey_spec/flakey_spec.rb:2" ])
+      expect(subject.find("./support_spec/flakey_spec.rb")).to eq([ "./support_spec/flakey_spec.rb:2" ])
     end
   end
 
