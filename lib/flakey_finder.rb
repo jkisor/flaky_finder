9922c85c8dd573ccf3693b3c0096adb7aa32115c
John Kisor <jkisor@gmail.com>
2021-10-08 08:02:14 -0400

    auto-commit

--- lib/flakey_finder.rb
+++ lib/flakey_finder.rb
@@ -8,18 +8,13 @@ require "./lib/flakey_finder/example"
 class FlakeyFinder
 
   def find(path, sample_size = 10)
-    runs = collect_runs(path, sample_size)
-    runs.unstable_examples
-  end
-
-  private
-
-  def collect_runs(path, sample_size)
     Runs.new(
       (1..sample_size)
         .map { |_| `bundle exec rspec #{path} --format j` }
         .map { |results| JSON.parse(results).fetch("examples") }
         .map { |examples| Run.new(examples) }
-    )
+    ).unstable_examples
   end
+
+
 end
